6dee5aafa8c682836c6258a7c07923236b649a26
Bill Middelbosch <b.middelbosch@isalu.nl>
2023-02-22 15:57:08 +0100

    Logo img update
    Changed img from local to full redirect to S3 bucket to be able to build it on Forge server.

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -2,7 +2,8 @@
 
   <div class="logo">
     <router-link :to="{ name: 'homepage.index' }">
-      <img src="@/../assets/Jumba logo-klein.png" alt="Jumba-Logo" width="100" height="100">
+      <!-- <img src="@/../assets/Jumba logo-klein.png" alt="Jumba-Logo" width="100" height="100"> -->
+      <img src="https://jumba-assets.s3.eu-west-1.amazonaws.com/Jumba-icon-klein.png" alt="Jumba-Logo" width="100" height="100">
 
     </router-link>
   </div>
