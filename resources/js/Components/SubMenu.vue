35a955ad4bd884e916f080dcde776f56c1db5cd5
Bill Middelbosch <b.middelbosch@isalu.nl>
2023-03-07 21:53:40 +0100

    Solve build error

--- resources/js/Components/SubMenu.vue
+++ resources/js/Components/SubMenu.vue
@@ -14,7 +14,7 @@ defineProps({
 
             <div class="grid grid-col-14 border-b-8 border-[#F3F4F6]">
                 <span class="col-start-2 col-span-6 space-x-12 py-6"> 
-                    <span v-for="item in items" class="p-4 hover:bg-[#E74650]"> {{ item }} </span>
+                    <span v-for="(item, index) in items" class="p-4 hover:bg-[#E74650]" :key="index"> {{ item }} </span>
                 </span>
                 <span class="col-start-8 col-span-2 py-6">Alle media</span>
             </div>
